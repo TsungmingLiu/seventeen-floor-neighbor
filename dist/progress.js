26283603a90b7e0a607da210c9ec0bc351b6ac88
TsungMing Liu <ddarliu329@gmail.com>
2026-09-24 20:08:18 -0400

    Commit generated Opening Chapter 1 demo output

--- dist/progress.js
+++ dist/progress.js
@@ -1,4 +1,4 @@
-import { memoryEventForNode } from './memories.js?v=05dbfcdbdd21';
+import { memoryEventForNode } from './memories.js?v=a3fa985a9bee';
 
 // Snapshots store node-entry state: choices are applied only when the player chooses.
 export class ProgressStore {
